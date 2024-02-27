1d3ce0fcb49f6a7cd5086de29d9194c6b3a988ca
hyuri <hyuri.miranda.cortes@gmail.com>
2024-02-27 15:28:28 -0300

    feat: coordenas create user

--- src/contollers/useController.ts
+++ src/contollers/useController.ts
@@ -2,8 +2,30 @@ import { Request, Response } from 'express';
 import * as userService from '../services/useService'; 
 import { STATUS } from '../types/Status';
 
+import geocoder from '../geocoder/geocoderConfig';
+
 const createUser = async (req: Request, res: Response) => {
   try {
+    const { address, coordinates }: { name: string; email: string; address?: string; coordinates?: { lat: number; lng: number; } } = req.body;
+
+    if ((address && coordinates) || (!address && !coordinates)) {
+      res.status(400).send({ message: 'Please provide either an address or coordinates, but not both or neither.' });
+      return;
+    }
+
+    let finalCoordinates = coordinates;
+    if (address && !coordinates) {
+      const geoResponse = await geocoder.geocode(address);
+      if (geoResponse.length > 0) {
+        finalCoordinates = { lat: Number(geoResponse[0].latitude), lng: Number(geoResponse[0].longitude) };
+      } else {
+        res.status(400).send({ message: 'Address not found.' });
+        return;
+      }
+    }
+
+    req.body.coordinates = finalCoordinates
+
     const user = await userService.createUser(req.body);
     res.status(STATUS.CREATED).json(user);
   } catch (error: unknown) {
