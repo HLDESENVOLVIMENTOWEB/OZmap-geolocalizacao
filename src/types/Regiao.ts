8a36575c32b0f841afe4241910011e54624afba2
hyuri <hyuri.miranda.cortes@gmail.com>
2024-02-28 15:32:10 -0300

    fix: ajustes região

--- src/types/Regiao.ts
+++ src/types/Regiao.ts
@@ -1,13 +1,11 @@
 import { Types } from "mongoose";
 
-// types.ts
 export interface Region {
-    _id?: Types.ObjectId | string;
-    name: string;
-  }
-  
-  export interface Point {
-    latitude: number;
-    longitude: number;
-  }
-  
+  _id?: Types.ObjectId | string;
+  name: string;
+}
+
+export interface Point {
+  latitude: number;
+  longitude: number;
+}
